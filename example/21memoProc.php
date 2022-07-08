1cd8bed0d41fa3a60edb4fb59151ffda80d255d9
pjw-redt <pjiww4@gmail.com>
2022-07-08 21:54:22 +0900

    today

---
+++ example/21memoProc.php
@@ -0,0 +1,17 @@
+<?php
+    include "21Lib.php";
+
+    $a = $_GET['name'];
+    $b = $_GET['memo'];
+    $c = date("Y-m-d H:i:s");
+
+    $query = "insert into memo(name, memo, regdate)
+            values('$a', '$b', '$c')";
+
+    mysqli_query($connect, $query);
+
+?>
+
+<script>
+    location.href='21memo.php'; //뒤로 가기!!
+</script>
