8380a75f193d47f4e6951f53d903b5e642693ef7
pjw-redt <pjiww4@gmail.com>
2022-08-26 13:13:33 +0900

    study0826_2

--- example/login/memberList.php
+++ example/login/memberList.php
@@ -1,26 +1,3 @@
-<!DOCTYPE html>
-<html lang="ko">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width-device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-
-<nav class="navbar navbar-dark bg-primary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/index.php">
-      <img src="/img/CAUtionImg.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
-      WebHell
-    </a>
-    <div style="float:right">
-      <a class="btn btn-primary" href="CAUtion.php" role="button">CAUtion Fighting^0^</a>
-      <a class="btn btn-primary" href="logOut.php" role="button">LogOut</a>
-    </div>
-
-  </div>
-</nav>
 
 <?php
 
